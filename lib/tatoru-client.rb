40936d69ef5556a88dd37cc6c33ebe28f393694a
Roberto Vasquez Angel <rva@beegoodit.de>
2021-07-19 02:30:36 +0200

    Cleanup.

--- lib/tatoru-client.rb
+++ lib/tatoru-client.rb
@@ -1,1 +1,20 @@
-require 'tatoru_client'
+require "singleton"
+require "httparty"
+
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+loader.ignore "*~"
+loader.ignore "#{__dir__}/tatoru-client.rb"
+loader.setup
+
+module Tatoru
+  module Client
+    def self.configure
+      yield Configuration
+    end
+
+    def self.root
+      Pathname.new(::File.dirname(__dir__))
+    end
+  end
+end
